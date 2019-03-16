b64356973e3cdafc8f405cec96ef35a79d0015c4
Your Name <you@example.com>
2019-03-16 02:28:04 -0500

    footer

---
+++ footer.php
@@ -0,0 +1,33 @@
+<div class="container-fluid" style="padding: 0px; margin-top: 30px;margin-bottom: 30px;">
+	<!--Menú -->
+	<div class="col-md-10 offset-md-1" style="padding-top: 25px;">
+		<nav class="navbar navbar-expand-md ">
+		    <a class="navbar-brand" href="#">
+		    	<img src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/Grupo-2.png" alt="">
+		    </a>
+		    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		    <div class="collapse navbar-collapse" id="navbarCollapse">
+		      <ul class="navbar-nav mr-auto">
+		        <li class="nav-item active">
+		          <a class="nav-link" href="#"  style="color: black;">Home <span class="sr-only">(current)</span></a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" href="#" style="color: black;" >Link</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" href="#" style="color: black;" >Disabled</a>
+		        </li>
+		      </ul>
+		        <div class="form-inline mt-2 mt-md-0">
+			        <a href="#"><img class="btn  my-2 my-sm-0" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/Grupo-3.png" alt=""></a>
+			        <a href="#"><img class="btn  my-2 my-sm-0" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/Grupo-4.png" alt=""></a>
+			        <a href="#"><img class="btn  my-2 my-sm-0" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/Grupo-5.png" alt=""></a>
+		      </div>
+		    </div>
+		</nav>
+	</div>
+</div>	
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php include "header.php"; ?>
 <?php include "parte1index.php"; ?>
 <?php include "parte2index.php"; ?>
+<?php include "footer.php"; ?>
