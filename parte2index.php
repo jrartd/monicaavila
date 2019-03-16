232fc594b56e658a033c3187635777d69a998df9
Your Name <you@example.com>
2019-03-16 02:21:52 -0500

    seccione

--- parte2index.php
+++ parte2index.php
@@ -1,17 +1,108 @@
 <!-- Info block 1 -->
-<section class="info-section">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6">
-				<h1>Lo màs reciente</h1>
+<section class="info-section" style="margin-top: 90px;">
+	<div class="container-fluid">
+		<div class="col-md-10 offset-md-1 row">
+			<div class="col-md-8">
+				<h1 class="titulobase col-md-12" style="font-size: 105px;color: black;font-weight: 700;letter-spacing: -8px;">Lo más reciente</h1>
+				<div class="row">
+					<div></div>
+					<img class="col-md-10 offset-md-1 img-fluid" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/una.jpg" alt="">				
+				</div>
 			</div>
-			<div class="col-md-6">
-				
+			<div class="col-md-4">
+				<div class="row">
+					<div class="col-md-12" style="padding: 0px;">
+						<img class="col-md-12 img-fluid" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/una.jpg" alt="">
+					</div>
+					<div class="col-md-12" style="padding: 0px;">
+						<img class="col-md-12 img-fluid" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/una.jpg" alt="">
+					</div>			
+				</div>
 			</div>
 		</div>
 	</div>
 </section>
 
+
+<!-- Info block 1 -->
+<section class="info-section" style="margin-top: 90px;">
+	<div class="container-fluid">
+		<div class="col-md-10 offset-md-1 row">
+			<h1 class="titulobase col-md-12" style="font-size: 70px;color: black;font-weight: 700;letter-spacing: -3px;">Departamentos</h1>
+			<h2 class="titulobase col-md-12" style="font-size: 40px;color: black;" >Espacios únicos donde puedes ser tú</h2>
+			<div class="col-md-4">
+				<div class="row">
+					<img class="col-md-12 img-fluid" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/photo-1502005097973-6a7082348e28.jpg" alt="">				
+				</div>
+			</div>
+			<div class="col-md-8">
+				<div class="row">
+					<div class="col-md-6 estiloloop" style="padding: 0px;">
+						<img class="col-md-12 img-fluid" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/nathan-fertig-249917-unsplash.jpg" alt="">
+						<h1 class="">Departamentos</h1>
+						<h2>Una vista para comtemplar</h2>
+					</div>
+					<div class="col-md-6 estiloloop" style="padding: 0px;">
+						<img class="col-md-12 img-fluid" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/marion-michele-770316-unsplash.jpg" alt="">
+						<h1 class="">Departamentos</h1>
+						<h2>Una vista para comtemplar</h2>
+					</div>
+					<div class="col-md-6 estiloloop" style="padding: 0px;">
+						<img class="col-md-12 img-fluid" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/photo-1494526585095-c41746248156.jpg" alt="">
+						<h1 class="">Departamentos</h1>
+						<h2>Una vista para comtemplar</h2>
+					</div>
+					<div class="col-md-6 estiloloop" style="padding: 0px;">
+						<img class="col-md-12 img-fluid" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/photo-1512916194211-3f2b7f5f7de3.jpg" alt="">
+						<h1 class="">Departamentos</h1>
+						<h2>Una vista para comtemplar</h2>
+					</div>			
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<!-- Info block 1 -->
+<section class="info-section" style="margin-top: 90px;">
+	<div class="container-fluid">
+		<div class="col-md-10 offset-md-1 row">
+			<h1 class="titulobase col-md-12" style="font-size: 70px;color: black;font-weight: 700;letter-spacing: -3px;">Casas</h1>
+			<h2 class="titulobase col-md-12" style="font-size: 40px;color: black;" >Espacios únicos donde puedes ser tú</h2>
+
+			<div class="col-md-8">
+				<div class="row">
+					<div class="col-md-6 estiloloop" style="padding: 0px;">
+						<img class="col-md-12 img-fluid" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/nathan-fertig-249917-unsplash.jpg" alt="">
+						<h1 class="">Departamentos</h1>
+						<h2>Una vista para comtemplar</h2>
+					</div>
+					<div class="col-md-6 estiloloop" style="padding: 0px;">
+						<img class="col-md-12 img-fluid" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/marion-michele-770316-unsplash.jpg" alt="">
+						<h1 class="">Departamentos</h1>
+						<h2>Una vista para comtemplar</h2>
+					</div>
+					<div class="col-md-6 estiloloop" style="padding: 0px;">
+						<img class="col-md-12 img-fluid" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/photo-1494526585095-c41746248156.jpg" alt="">
+						<h1 class="">Departamentos</h1>
+						<h2>Una vista para comtemplar</h2>
+					</div>
+					<div class="col-md-6 estiloloop" style="padding: 0px;">
+						<img class="col-md-12 img-fluid" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/photo-1512916194211-3f2b7f5f7de3.jpg" alt="">
+						<h1 class="">Departamentos</h1>
+						<h2>Una vista para comtemplar</h2>
+					</div>			
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="row">
+					<img class="col-md-12 img-fluid" src="https://monicaavilainmobiliaria.com/wp-content/uploads/2019/03/photo-1502005097973-6a7082348e28.jpg" alt="">				
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
 
 
